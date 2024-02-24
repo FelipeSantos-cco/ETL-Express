bed103d6523421b7d4ce56b12ce716a170b4e9f1
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-02-24 02:23:41 -0300

    :construction: Feat: Add new script

---
+++ main.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog="ETL Express",
+                                 description='''
+                                 \n<-> ETL Express <->
+                                 \n[*] Fast ETL, like an espresso\n
+                                 \n[*] Program to facilitate the ETL process via command line\n
+                                 \n[@] Felipe Santos -> https://github.com/FelipeSantos-cco/
+                                 ''')
