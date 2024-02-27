55fc5543836eb6bd9e1ca1d88ddc6c6a80de0328
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-02-27 15:05:34 -0300

    :bug: Fix: xlsx_to_df Function

--- extract/extract_local.py
+++ extract/extract_local.py
@@ -10,10 +10,11 @@ def csv_to_df(csv_path):
     else:
         return None 
 
+# Extract from XLSX to a DF
 def xlsx_to_df(xlsx_path):
 
     if is_exists_file(xlsx_path):
-        df = pd.read_csv(xlsx_path)
+        df = pd.read_excel(xlsx_path)
         return df
     
     else:
