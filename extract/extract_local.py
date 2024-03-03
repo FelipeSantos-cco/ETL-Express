64f2451a0bd7dba87442572e45a1068b35d237b3
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-03-03 00:28:37 -0300

    :recycle: Refactor: Remove redundancies

--- extract/extract_local.py
+++ extract/extract_local.py
@@ -7,47 +7,23 @@ def controller(file):
         extension = extension[1]
     
         if extension == ".csv":
-            return csv_to_df(file)
+            df = pd.read_csv(file) # Extract from CSV to a DF
         
         elif extension == ".xlsx":
-            return xlsx_to_df(file)
+            df = pd.read_excel(file) # Extract from XLSX to a DF
         
         elif extension == ".json":
-            return json_to_df(file)
+            df = pd.read_json(file) # Extract from JSON to a DF
         
         else:
             print("[!] File Not Supported")
+            return None
+        
+        return df
 
     else:
         return None
 
-# Extract from CSV to a DF
-def csv_to_df(csv_path):
-
-    if is_exists_file(csv_path):
-        df = pd.read_csv(csv_path)
-        return df
-    else:
-        return None 
-
-# Extract from XLSX to a DF
-def xlsx_to_df(xlsx_path):
-
-    if is_exists_file(xlsx_path):
-        df = pd.read_excel(xlsx_path)
-        return df
-    
-    else:
-        return None  
-
-# Extract from JSON to a DF
-def json_to_df(json_path):
-    
-    if is_exists_file(json_path):
-        return  pd.read_json(json_path)
-        
-    else:
-        return None  
 
 # is there an file?
 def is_exists_file(file_path):
